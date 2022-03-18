4593d607e46b80732f6e5d05dd51731278b46a59
lcole26 <lcole26@uic.edu>
2022-03-18 18:32:03 -0500

    a bit of additional js functionality banged out. also a bit of reorganization of files

--- js_stuff/global_variables.js
+++ js_stuff/global_variables.js
@@ -5,3 +5,6 @@
 // message formatting stuff
 var msgFormat = "<p class={user}>{msg}</p>";
 // var msgSentAudio =
+var msgReceiveSound = SetUpMsgReceivedAudio();
+
+// export { msgFormat, msgReceiveSound };
--- js_stuff/main.js
+++ js_stuff/main.js
@@ -11,24 +11,30 @@
  * References Cited:
  * https://stackoverflow.com/questions/14926306/javascript-play-sound-on-hover-stop-and-reset-on-hoveroff
  *
+ * Message posting stuff ref:
+ *  https://stackoverflow.com/questions/42158073/how-to-create-and-append-text-in-the-body-using-javascript
+ *  https://stackoverflow.com/questions/4851699/setting-the-id-attribute-of-an-input-element-dynamically-in-ie-alternative-for
+ *  https://www.codegrepper.com/code-examples/javascript/how+to+add+a+paragraph+in+html+using+javascript
+ *
  */
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------
 /**
  * getter/setter stuff
  */
+
 var msg_end_marker = "+++";
 var sms_msg_format =
   '<p class="user.{username}">{msg}<br><br> {msg_end_marker}</p>';
 var sms_received_sound;
 const sound_urls = {
-  sms_received_sound_: "/media/audio/545372__stwime__up3.ogg",
+  sms_received_sound_: "../media/audio/545372__stwime__up3.ogg",
 };
 
 const users = {
-  klause: "klause",
-  boma: "boma",
-  secondwind: "secondwind",
-  harakiri_salaryman: "harakiri_salaryman",
+  klause: "user_klause",
+  boma: "user_boma",
+  secondwind: "user_secondwind",
+  harakiri_salaryman: "user_harakiri_salaryman",
 };
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -51,7 +57,10 @@ function getLocalStream() {
       console.log("u got an error:" + err);
     });
 }
-
+/**
+ *
+ * @returns
+ */
 var SetUpMsgReceivedAudio = function () {
   return new Howl({
     src: [
@@ -66,6 +75,11 @@ var SetUpMsgReceivedAudio = function () {
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+/**
+ *
+ * @param {*} itemName
+ * @param {*} item
+ */
 function AddSessionStorage(itemName, item) {
   sessionStorage.setItem(itemName, item);
 }
@@ -80,6 +94,21 @@ function StrikeText(elem_id) {
   document
     .getElementById(elem_id)
     .style.setProperty("text-decoration", "line-through");
+}
+
+/**
+ *
+ * @param {*} msg_id
+ * @param {*} timeout_factor
+ */
+function unhideMessageonTimer(msg_id, timeout_factor) {
+  let msg = document.getElementById(msg_id);
+  setTimeout(() => {
+    if (msg != null && msg.getAttribute("hidden").equals(true)) {
+      msg.setAttribute("hidden", false);
+    }
+  }, timeout_factor);
+  // if (msg != null && msg.getAttribute("hidden").equals("hidden")) {
 }
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -111,33 +140,61 @@ var format = function (str, col) {
   });
 };
 
-var newMessage = function (
-  message_marker_elem_id,
+/**
+ *
+ * @param {*} message_marker_elem_id
+ * @param {*} user_class
+ * @param {*} message_id
+ * @param {*} msg_end_marker
+ * @param {*} time_offset
+ * @param {*} msg_sound
+ */
+var postNewMessage = function (
+  user_class,
+  new_message_id,
+  time_offset,
+  user_msg,
+  msg_sound
+) {
+  // let end_mark = document.getElementById(message_marker_elem_id);
+  // let user_msg = document.getElementById(user_msg);
+  // let msg = format(sms_msg_format, {
+  //   username: user,
+  //   msg: user_msg,
+  //   msg_end_marker: msg_end_marker,
+  // });
+
+  let new_paragraph_obj = document.createElement("p");
+  let new_text_node_obj = document.createTextNode(user_msg);
+
+  setTimeout(() => {
+    new_paragraph_obj.id = new_message_id;
+    new_paragraph_obj.className = user_class;
+    new_paragraph_obj.appendChild(new_text_node_obj);
+    document.body.appendChild(new_paragraph_obj);
+    msg_sound.play();
+  }, time_offset * 1000);
+};
+
+/**
+ * 
+ * @param {
+ } message_marker_elem_id 
+ * @param {*} user_class 
+ * @param {*} message_id 
+ * @param {*} msg_end_marker 
+ * @param {*} time_offset 
+ * @param {*} msg_sound 
+ */
+var appendNewMessage = function (
   user_class,
   message_id,
   msg_end_marker,
   time_offset,
   msg_sound
 ) {
-  // let end_mark = document.getElementById(message_marker_elem_id);
-  // let user_msg = document.getElementById(user_msg);
-  let msg = format(sms_msg_format, {
-    username: user,
-    msg: user_msg,
-    msg_end_marker: msg_end_marker,
-  });
-
-  let new_paragraph_obj = document.createElement("p");
-  if (message_id != null) {
-    new_paragraph_obj.setAttribute("id", message_id);
-  }
-  new_paragraph_obj.setAttribute("class", user_class);
-  let new_text_node_obj = document.createTextNode(user_msg);
-  let f = document.crea;
-
-  setTimeout(() => {
-    // end_mark.innerHTML += user_msg;
-  }, time_offset);
+  let msg = document.getElementById(user_class);
+  document.getElementById(user_class).innerHTML += msg;
 };
 
 function newMessageWithLink() {
